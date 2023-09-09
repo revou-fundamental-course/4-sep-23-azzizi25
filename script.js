fc2fde32f54faaf6d53cc80b9e2988038d3804d2
Dicka Rifqi <azzizi0202@gmail.com>
2023-09-09 15:07:53 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const display = document.getElementById("display");
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      if (button.id === "clear") {
+        display.value = "";
+      } else if (button.id === "backspace") {
+        display.value = display.value.slice(0, -1);
+      } else if (button.id === "calculate") {
+        try {
+          display.value = eval(display.value);
+        } catch (error) {
+          display.value = "Error";
+        }
+      } else {
+        display.value += button.innerText;
+      }
+    });
+  });
+});
